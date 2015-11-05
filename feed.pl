7404617883480c25521e8da6dbae688519398471
m171818 <m171818@usna.edu>
2015-11-05 07:26:13 -0500

    Updates
    Removed duplicates
    Tab delimiting split on the status file

--- feed.pl
+++ feed.pl
@@ -2,7 +2,8 @@
 use strict;
 use CGI qw( :standard );
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
-
+###Testing
+use Data::Dumper qw(Dumper);
 my $expires = gmtime(time() + 86400);
 ###For testing only###                                                                                                                                                                                
 print "Set-Cookie: user=test; expires=$expires; \n";
@@ -20,6 +21,10 @@ open(my $myfile,  $fname);
 while(my $line = <$myfile>) {
     chomp $line;
     print("<div class = \"post\"><p>$user posted a status</p><p>$line</p></div>");
+    ###Splits the array on tabs. 
+    my @words = split /\t/, $line;
+    ###This prints out the array - for easy reading
+    print Dumper \@words;
 }
 
 print end_html();
