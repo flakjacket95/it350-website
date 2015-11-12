3e7f95e0e675e36b121c75d308d13dab03ac8d83
MIDN DANIEL FLACK {SIT} <m171818@usna.edu>
2015-11-12 07:29:36 -0500

    Added get posts functionality

---
+++ get-posts.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+use CGI qw( :standard );
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+###Testing
+use CGI::Session;
+
+# Start the session.                                                                                                                                                                                         
+# This reads the cookies and resumes a previous session if present.                                                                                                                                          
+my $session = new CGI::Session("driver:File", undef, {Directory=>'/tmp'});
+my $sid = $session->id();
+
+# Get the username from the current session                                                                                                                                                                  
+# This was set with the login.pl script.                                                                                                                                                                     
+my $username = $session->param('username');
+###Checks for no-login and returns to the homepage if so                                                                                                                                                     
+if(!($username)) {
+    print "Location: index.html\n\n"
+}
+print header();
+print start_html("Status's");
+my $frnd = "friend/".$username.".txt";
+open(my $myfile,  $frnd);
+while(my $line = <$myfile>) {
+    chomp $line;
+    print("<div class = \"post\"><p>$username posted a status</p><p>$line</p></div>");
+    ###Splits the array on tabs.                                                                                                                     
+
+    my $fname = "status/".$username.".txt";
+    
+    print ("<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\">"); 
+    print ("<div class=\"container\">");
+    
+    open(my $myfile,  $fname);
+    while(my $line = <$myfile>) {
+	chomp $line;
+	print("<div class = \"post\"><p>$username posted a status</p><p>$line</p></div>");
+	###Splits the array on tabs. 
+	my @words = split /\t/, $line;
+    }
+    
+    print ("</div>");
+}
+print end_html();
